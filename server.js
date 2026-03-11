bb720322d189403da354d03547e304818cc156db
turtleman1278 <schen7812@gmail.com>
2026-03-11 01:26:50 -0400

    added a zip code to state abbrv and added the alert api

--- server.js
+++ server.js
@@ -22,22 +22,38 @@ app.get(`/api/weather`, async (req, res) =>{
         } 
         
         //Geolocator API- gets our longitude and latitude 
-        const locationURL = new URL(  
-          `https://api.openweathermap.org/geo/1.0/zip`
-        );
+        const locationURL = new URL(`https://api.openweathermap.org/geo/1.0/zip`);
         locationURL.searchParams.set("zip", `${zip},US`);
         locationURL.searchParams.set("appid", api_key);
         const locationResponse = await fetch(locationURL);
         const locationData = await locationResponse.json();
 
-        if(!locationResponse.ok){
-            return res.status(locationResponse.status).json({
-                message: locationData.message || "Error, could not pull location data."
-            });
-        } 
+        if (!locationResponse.ok) {
+          return res.status(locationResponse.status).json({
+            message:
+              locationData.message || "Error, could not pull location data.",
+          });
+        }
 
+        //gets out longitude and latitude
         const longData = locationData.lon;
         const latData = locationData.lat; 
+
+
+        //Zip code to state for the weather alert API 
+        const stateCodeURL = new URL(`https://api.zippopotam.us/us/${zip}`);
+        const stateCodeResponse = await fetch(stateCodeURL);
+        const stateCodeResult = await stateCodeResponse.json(); 
+
+        if (!stateCodeResponse.ok) {
+          return res.status(stateCodeResponse.status).json({
+            message: "Error, could not pull state code data.",
+          });
+        }
+
+
+        const stateCode = stateCodeResult.places[0]["state abbreviation"];
+
 
         //current weather data API 
         const weatherCurrentURL = new URL(`https://api.openweathermap.org/data/2.5/weather?lat=${latData}&lon=${longData}`);
@@ -45,28 +61,52 @@ app.get(`/api/weather`, async (req, res) =>{
         weatherCurrentURL.searchParams.set("appid", api_key);
         const weatherCurrentResponse = await fetch(weatherCurrentURL);
         const weatherCurrentData = await weatherCurrentResponse.json(); 
-        if(!weatherCurrentResponse.ok){
-            return res.status(weatherCurrentResponse.status).json({
-                message: weatherCurrentData.message || "Error, could not pull current weather data."
-            });
+
+        if (!weatherCurrentResponse.ok) {
+          return res.status(weatherCurrentResponse.status).json({
+            message:
+              weatherCurrentData.message ||
+              "Error, could not pull current weather data.",
+          });
         } 
 
+        
         //Forcast API
         const weatherForcastURL = new URL(`https://api.openweathermap.org/data/2.5/forecast?lat=${latData}&lon=${longData}`);
         weatherForcastURL.searchParams.set("units", "imperial");
         weatherForcastURL.searchParams.set("appid", api_key);
         const weatherForcastResponse = await fetch(weatherForcastURL);
         const weatherForcastData = await weatherForcastResponse.json();
-        if(!weatherForcastResponse.ok){
-            return res.status(weatherForcastResponse.status).json({
-                message: weatherForcastData.message || "Error, could not pull forecast data."
-            });
-        } 
 
+        if (!weatherForcastResponse.ok) {
+          return res.status(weatherForcastResponse.status).json({
+            message:
+              weatherForcastData.message ||
+              "Error, could not pull forecast data.",
+          });
+        } 
+        
+
+        //weather alerts
+        const weatherAlertURL = new URL(`https://api.weather.gov/alerts/active?area=${stateCode}`);
+        const weatherAlertResponse = await fetch(weatherAlertURL);
+        const weatherAlertData = weatherAlertResponse.json(); 
+
+        if (!weatherAlertResponse.ok) {
+          return res.status(weatherAlertResponse.status).json({
+            message: "Error, could not pull weather alerts.",
+          });
+        }
+
+
+        //top-level keys
         return res.json({
             location: locationData,
             current: weatherCurrentData,
-            forecast: weatherForcastData
+            forecast: weatherForcastData,
+            state: stateCode,
+            alert: weatherAlertData,
+
         });
 
     } catch(err) {
